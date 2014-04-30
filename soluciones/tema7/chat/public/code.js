3bdc4398df6fc5609cd0fb3a9888789eca359e7d
Elías Alonso <werelax@users.noreply.github.com>
2014-04-30 16:59:39 +0200

    tema7/chat adaptado a express4

--- soluciones/tema7/chat/public/code.js
+++ soluciones/tema7/chat/public/code.js
@@ -4,15 +4,15 @@ $(function() {
       me = $.get("/me");
 
   Chat.registerHandler(function(msg) {
-    var msgData = {text: msg, date: new Date()}
+    var msgData = { text: msg, date: new Date() };
     me.then(function(me) {
       socket.emit("send:message", me, msgData);
       Chat.showMyMsg(me, msgData);
-    })
-  })
+    });
+  });
 
   socket.on("posted:message", function(user, msgData) {
     Chat.postMsg(user, msgData);
-  })
+  });
 
 });
---
+++ tema7/chat/public/code.js
@@ -0,0 +1,5 @@
+$(function() {
+
+  // ---8<--- El cliente del chat aqui! ---8<---
+
+});
